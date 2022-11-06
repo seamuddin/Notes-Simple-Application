9998d030c880605610af7bf2178446322721e54e
seamuddin <seamuddin2020@gmail.com>
2022-11-06 21:53:49 +0600

    Add & Modification - Home page introduce - Root url added for home page - Requiriments.txt updated with new packeges

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
 from src import create_app
-
-
+from flask import Flask, jsonify, render_template
+from . import database
+app = Flask(__name__)
 application = create_app()
+
+
+
+@app.get("/")
+def say_hello():
+    return render_template('home.html')
